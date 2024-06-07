819d8954186ee170327d71addfc53b7ec683963a
ehinrichs <54997013+ehinrichs@users.noreply.github.com>
2024-06-07 10:06:45 -0600

    site updates to improve gallery
    site updates to improve gallery

--- src/components/image-gallery/Gallery.js
+++ src/components/image-gallery/Gallery.js
@@ -1,61 +1,29 @@
-import React, { useState } from "react";
-import { LightBox } from "react-lightbox-pack";
-import "react-lightbox-pack/dist/style.css";
+import React from "react";
+import LightGallery from "lightgallery/react";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-thumbnail.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-autoplay.css";
+import "lightgallery/css/lg-fullscreen.css";
 import "./styles.css";
 
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import lgAutoplay from "lightgallery/plugins/autoplay";
+import lgFullscreen from "lightgallery/plugins/fullscreen";
+
 const PhotoGallery = ({ photos, title }) => {
-  const [toggle, setToggle] = useState(false);
-  const [sIndex, setSIndex] = useState(0);
-
-  const lightBoxHandler = (state, index) => {
-    setToggle(state);
-    setSIndex(index);
-  };
-
-  const handleKeyDown = (event, index) => {
-    if (event.key === "Enter" || event.key === " ") {
-      lightBoxHandler(true, index);
-    }
-  };
-
   return (
-    <div >
-      <h2>{title}</h2>
-      <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
-        {photos.map((item, index) => (
-          <div className="justified-gallery"
-            key={item.id}
-            style={{ display: "inline-block", margin: "5px", cursor: "pointer" }}
-            onClick={() => lightBoxHandler(true, index)}
-            onKeyDown={(event) => handleKeyDown(event, index)}
-            tabIndex={0}
-            role="button"
-            aria-label={`View ${item.title}`}
-          >
-            <img className="gallery-img"
-              src={item.image}
-              alt={item.title}
-              style={{ width: "199px" , height: "135px"}}
-            />
-          </div>
-        ))}
-      </div>
+    <div>
+    <h2>{title}</h2>
 
-      <LightBox
-        state={toggle}
-        event={lightBoxHandler}
-        data={photos.map((item) => ({
-          id: item.id,
-          image: item.image,
-          title: item.title,
-        }))}
-        imageWidth="60vw"
-        imageHeight="70vh"
-        thumbnailHeight={50}
-        thumbnailWidth={50}
-        setImageIndex={setSIndex}
-        imageIndex={sIndex}
-      />
+    <LightGallery speed={1000} plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen]} mode="lg-fade">
+      {photos.map((item) => (
+        <a key={item.id} href={item.image} data-sub-html={`<h4>${item.title}</h4>`}>
+          <img className="gallery-img" src={item.image} alt={item.title} style={{width: "225px", height: "165px", border: "3px solid #0e6e9b", margin: "5px", borderRadius: "10px"}} />
+        </a>
+      ))}
+    </LightGallery>
     </div>
   );
 };
